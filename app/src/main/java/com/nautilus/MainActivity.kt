89f6090991f9d58e60f3f2403845cecff04e9bc4
sulig <suligvondooka@gmail.com>
2025-07-07 19:01:29 +0200

    ALL YOUR FRIENDS [[Abandoned you for the slime]]?

--- app/src/main/java/com/nautilus/MainActivity.kt
+++ app/src/main/java/com/nautilus/MainActivity.kt
@@ -7,6 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,13 +28,26 @@ class MainActivity : AppCompatActivity() {
         val btnEntry = findViewById<Button>(R.id.btnEntry)
         val btnExit = findViewById<Button>(R.id.btnExit)
 
+        // --
         btnEntry.setOnClickListener {
            val actMenu = Intent(this, MainMenu::class.java)
             startActivity(actMenu)
         }
-
         btnExit.setOnClickListener {
             finish()
         }
+        //-***************-
+
+        /** Coroutine -->*/
+        CoroutineScope(Dispatchers.Main).launch {
+            // Wait 3 seconds
+            delay(TimeUnit.SECONDS.toMillis(3))
+            withContext(Dispatchers.Main) {
+                // Start the MainMenu activity
+                val actMenu = Intent(this@MainActivity, MainMenu::class.java)
+                startActivity(actMenu)
+            }
+        }
+        /***************************************/
     }
 }
