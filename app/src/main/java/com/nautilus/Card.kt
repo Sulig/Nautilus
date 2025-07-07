6f8ed9d036677cb20f685cf2677f726739a89d92
sulig <suligvondooka@gmail.com>
2025-07-07 16:58:56 +0200

    ALL YOUR FRIENDS [[Abandoned you for the slime]]?

--- app/src/main/java/com/nautilus/Card.kt
+++ app/src/main/java/com/nautilus/Card.kt
@@ -31,73 +31,81 @@ class Card : AppCompatActivity() {
         val imgCardNum = findViewById<ImageView>(R.id.imgCardNum)
         val imgCard = findViewById<ImageView>(R.id.imgCard)
 
+        /**
+         * Recuperate the "btnNum" from the previous activity (MainMenu.kt)
+         *     to use it as which button is pressed
+         */
         val memo=intent.extras
         val btnClicked = memo?.getString("btnNum")
 
+        /**
+         * Depending on which button is clicked (previously in MainMenu.kt), the card will change
+         * with correct information.
+         */
         when (btnClicked) {
             "btn1" -> {
-                txtCardTitle.setText("@string/cardTitle1")
-                txtCardDes.setText("@string/cardDescription1")
+                txtCardTitle.text = getString(R.string.cardTitle1)
+                txtCardDes.text = getString(R.string.cardDescription1)
                 imgCardNum.setImageResource(R.drawable.numero1)
                 imgCard.setImageResource(R.drawable.img1__introduccion)
             }
             "btn2" -> {
-                txtCardTitle.text = "@string/cardTitle2"
-                txtCardDes.text = "@string/cardDescription2"
+                txtCardTitle.text = getString(R.string.cardTitle2)
+                txtCardDes.text = getString(R.string.cardDescription2)
                 imgCardNum.setImageResource(R.drawable.numero2)
                 imgCard.setImageResource(R.drawable.img2__acantilados)
             }
             "btn3" -> {
-                txtCardTitle.text = "@string/cardTitle3"
-                txtCardDes.text = "@string/cardDescription3"
+                txtCardTitle.text = getString(R.string.cardTitle3)
+                txtCardDes.text = getString(R.string.cardDescription3)
                 imgCardNum.setImageResource(R.drawable.numero3)
                 imgCard.setImageResource(R.drawable.img3__cala_pedrosa)
             }
             "btn4" -> {
-                txtCardTitle.text = "@string/cardTitle4"
-                txtCardDes.text = "@string/cardDescription4"
+                txtCardTitle.text = getString(R.string.cardTitle4)
+                txtCardDes.text = getString(R.string.cardDescription4)
                 imgCardNum.setImageResource(R.drawable.numero4)
                 imgCard.setImageResource(R.drawable.img4__foradada)
             }
             "btn5" -> {
-                txtCardTitle.text = "@string/cardTitle5"
-                txtCardDes.text = "@string/cardDescription5"
+                txtCardTitle.text = getString(R.string.cardTitle5)
+                txtCardDes.text = getString(R.string.cardDescription5)
                 imgCardNum.setImageResource(R.drawable.numero5)
                 imgCard.setImageResource(R.drawable.img5__tres_coves)
             }
             "btn6" -> {
-                txtCardTitle.text = "@string/cardTitle6"
-                txtCardDes.text = "@string/cardDescription6"
+                txtCardTitle.text = getString(R.string.cardTitle6)
+                txtCardDes.text = getString(R.string.cardDescription6)
                 imgCardNum.setImageResource(R.drawable.numero6)
                 imgCard.setImageResource(R.drawable.img6__montgo_)
             }
             "btn7" -> {
-                txtCardTitle.text = "@string/cardTitle7"
-                txtCardDes.text = "@string/cardDescription7"
+                txtCardTitle.text = getString(R.string.cardTitle7)
+                txtCardDes.text = getString(R.string.cardDescription7)
                 imgCardNum.setImageResource(R.drawable.numero7)
                 imgCard.setImageResource(R.drawable.img7_l_escala_)
             }
             "btn8" -> {
-                txtCardTitle.text = "@string/cardTitle8"
-                txtCardDes.text = "@string/cardDescription8"
+                txtCardTitle.text = getString(R.string.cardTitle8)
+                txtCardDes.text = getString(R.string.cardDescription8)
                 imgCardNum.setImageResource(R.drawable.numero8)
                 imgCard.setImageResource(R.drawable.img8__empuries_2)
             }
             "btn9" -> {
-                txtCardTitle.text = "@string/cardTitle9"
-                txtCardDes.text = "@string/cardDescription9"
+                txtCardTitle.text = getString(R.string.cardTitle9)
+                txtCardDes.text = getString(R.string.cardDescription9)
                 imgCardNum.setImageResource(R.drawable.numero9)
                 imgCard.setImageResource(R.drawable.img9__medes_emergides_2)
             }
             "btn10" -> {
-                txtCardTitle.text = "@string/cardTitle10"
-                txtCardDes.text = "@string/cardDescription10"
+                txtCardTitle.text = getString(R.string.cardTitle10)
+                txtCardDes.text = getString(R.string.cardDescription10)
                 imgCardNum.setImageResource(R.drawable.numero10)
                 imgCard.setImageResource(R.drawable.img10_medes_sumergidas)
             }
             "btn11" -> {
-                txtCardTitle.text = "@string/cardTitle11"
-                txtCardDes.text = "@string/cardDescription11"
+                txtCardTitle.text = getString(R.string.cardTitle11)
+                txtCardDes.text = getString(R.string.cardDescription11)
                 imgCardNum.setImageResource(R.drawable.numero11)
                 imgCard.setImageResource(R.drawable.img11_cierre)
             }
